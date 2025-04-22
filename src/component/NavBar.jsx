6f393fab8540c37b1bad7c1ae4af1596fc720434
MarcoVacchi <mvacchi816@gmail.com>
2025-04-23 00:16:52 +0200

    fix: img header

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -3,35 +3,34 @@ import { Link } from 'react-router-dom';
 
 export default function NavBar() {
 
-    return (
-        <>
-            <header>
-                <nav className="navbar navbar-expand-lg bg-black ">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#"><img src="/logo.png" alt="123" /></a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <Link to={"/"} className="nav-link text-white" > Home</Link >
-                                </li>
-                                <li className="nav-item">
-                                    <Link to={"/games"} className="nav-link text-white" > Games</Link >
+    return <div>
 
-                                </li>
-                                <li className="nav-item">
-                                    <Link to={"/about"} className="nav-link text-white" >Chi siamo</Link >
-                                </li>
+        <header>
+            <nav className="navbar navbar-expand-lg bg-black ">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#"><img src="/logo.png" alt="123" /></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link to={"/"} className="nav-link text-white" > Home</Link >
+                            </li>
+                            <li className="nav-item">
+                                <Link to={"/games"} className="nav-link text-white" > Games</Link >
 
-                            </ul>
-                        </div>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={"/about"} className="nav-link text-white" >Chi siamo</Link >
+                            </li>
+                        </ul>
                     </div>
-                </nav>
-            </header>
-            <Outlet />
-        </>
-    )
+                </div>
+            </nav>
+        </header>
+        <Outlet />
+
+    </div>
 };
 
